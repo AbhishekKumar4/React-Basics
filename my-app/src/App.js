3a92a686286dfb6fffc25132eeb5072727abeedd
Abhishek <kumarabhishek0388@gmail.com>
2020-05-06 18:43:44 +0530

    Class compo example

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Hello from './components/Hello'
+import HelloClassComp from './components/HelloClassComp'
 
 function App() {
   return (
     <div className="App">
       <Hello />
+      <HelloClassComp />
     </div>
   );
 }
---
+++ my-app/src/components/HelloClassComp.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class HelloClassComp extends Component {
+     render() {
+         return <h1>Hello from class component. </h1>
+     }
+}
+
+export default HelloClassComp;
