11ac0c1ca50b5d40757f8030c02301c80c89b045
Abhishek <kumarabhishek0388@gmail.com>
2020-05-07 21:34:42 +0530

    state object

--- my-app/src/components/Counter.js
+++ my-app/src/components/Counter.js
@@ -9,16 +9,29 @@ class Counter extends Component {
     }
 
     incrementValue() {
-        this.setState( {
+       /** this.setState( {
             counter : this.state.counter + 1
-        })
+        }, ()=> {console.log('Callback Value: ' + this.state.counter)})
+         */
+        this.setState(prevState => ( {
+            counter : prevState.counter +1
+        }))
+        console.log('Çounter value : ' + this.state.counter)
+    }
+
+    incrementFive() {
+        this.incrementValue()
+        this.incrementValue()
+        this.incrementValue()
+        this.incrementValue()
+        this.incrementValue()
     }
 
     render() {
         return(
             <div>
                 <h1>Counter Value : {this.state.counter}</h1>
-                <button onClick = { () => this.incrementValue()}>Increment</button>
+                <button onClick = { () => this.incrementFive()}>Increment</button>
             </div>
         )
     }
