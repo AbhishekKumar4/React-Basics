1ce89f5750f37ebb309ed7d681785dea02a9e410
Abhishek <kumarabhishek0388@gmail.com>
2020-05-07 18:32:21 +0530

    state object

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,15 +4,17 @@ import Hello from './components/Hello'
 import HelloClassComp from './components/HelloClassComp'
 import HelloPure from './components/HelloPureRct'
 import CustomProps from './components/CustomProps'
+import HelloStExample from './components/HelloStExample'
 
 function App() {
   return (
     <div className="App">
-      <Hello />
+     {/* <Hello />
       <HelloClassComp />
       <HelloPure />
-      <CustomProps test = "This is prop"> This is child text</CustomProps>
-    </div>
+      <CustomProps test = "This is prop"> This is child text</CustomProps>*/}
+    <HelloStExample />
+  </div>
   );
 }
 
